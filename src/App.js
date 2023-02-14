423ccb9aa086ced0c580ade86cf4a64da899aa12
Quentin Provent <quentin@mooveat.co>
2023-02-14 11:26:29 +0100

    Passing data throught props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from "react";
 
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
 export default function Board() {
   return (
     <React.Fragment>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
-    </React.Fragment> 
+      </React.Fragment> 
   );
 
-  /* Note : +0.5 pts à ce cellui qui comprend pourquoi
+  /* Note : +0.5 pts à ce celui qui comprend pourquoi
   je n'ai pas réussi à utiliser le "empty tag" à la place de React.Fragment */
+
 }
