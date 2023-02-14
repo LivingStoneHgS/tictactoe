d685208d0c1db5c97fa1c67f2eb9999d3d96fbe5
Quentin Provent <quentin@mooveat.co>
2023-02-14 16:07:49 +0100

    Upgrades #1 & 2

--- src/App.js
+++ src/App.js
@@ -31,27 +31,21 @@ function Board({ xIsNext, squares, onPlay }) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
-  return (
-    <React.Fragment>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </React.Fragment>
-  );
-}
+  const board = [0, 1, 2].map((row, i) => {
+    let line = [];
+    for (let j = 0; j < 3; j++) {
+        line.push(<Square value={squares[3*row+j]} onSquareClick={() => handleClick(3*row+j)}></Square>)
+    }
+    return <div className="board-row">{line}</div>
+    })
+
+    return (
+      <React.Fragment>
+        <div className="status">{status}</div>
+        { board }
+      </React.Fragment>
+    );
+  }
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
@@ -89,7 +83,10 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{moves}</ol>
+        <ol>
+          {moves}
+          <li>You are at move #{ history.length }</li>
+        </ol>
       </div>
     </div>
   );
